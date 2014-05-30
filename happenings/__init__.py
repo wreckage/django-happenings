fc14b90aaf843065d4abd5edf9b4244830cb74d3
Reuben Urbina <reubenurbina@gmail.com>
2014-05-30 03:34:17 -0700

    added version# and url to __init__. Updated setup.py

---
+++ happenings/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.1'
+__url__ = 'https://github.com/wreckage/django-happenings'
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup, find_packages
 
+import happenings
+
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
@@ -8,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-happenings',
-    version='0.1.1',
+    version=happenings.__version__,
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
@@ -16,11 +18,12 @@ setup(
     long_description=README,
     author='Reuben Urbina',
     author_email='reuben.urbina@gmail.com',
-    url='https://github.com/wreckage/django-happenings',
+    url=happenings.__url__,
     zip_safe=False,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
