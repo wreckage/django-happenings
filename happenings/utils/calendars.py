6906dc61a8b5cb30c3431754a016bbfef14e4b06
Reuben Urbina <reubenurbina@gmail.com>
2014-06-03 04:22:57 -0700

    cleaned up calendars.py

--- happenings/utils/calendars.py
+++ happenings/utils/calendars.py
@@ -10,9 +10,9 @@ from .common import now
 URL = getattr(settings, "CALENDAR_URL", 'calendar')
 
 
-class EventCalendar(LocaleHTMLCalendar):
+class GenericCalendar(LocaleHTMLCalendar):
     def __init__(self, year, month, count, all_month_events, *args):
-        super(EventCalendar, self).__init__(*args)
+        super(GenericCalendar, self).__init__(*args)
         self.yr = year
         self.mo = month
         self.count = count  # defaultdict in {date:[title1, title2,]} format
@@ -33,15 +33,33 @@ class EventCalendar(LocaleHTMLCalendar):
                 if cancellation.date == d:
                     self.title += " (CANCELLED)"
 
+    def formatday(self, day, weekday):
+        """Sets some commonly used variables."""
+        self.wkday_not_today = '<td class="%s"><div class="td-inner">' % (
+            self.cssclasses[weekday])
+
+        self.wkday_today = (
+            '<td class="%s calendar-today"><div class="td-inner">' % (
+                self.cssclasses[weekday])
+        )
+
+        self.anch = '<a href="/%s/%d/%d/%d">%d</a>' % (
+            URL, self.yr, self.mo, day, day
+        )
+
+        self.end = '</div></td>'
+
+class EventCalendar(GenericCalendar):
     def popover_helper(self):
         """Returns variables used to build popovers."""
+        # when
         self.when = ('<p><b>When:</b> ' + month_name[self.mo] + ' ' +
                      str(self.day) + ', ' + self.event.l_start_date.strftime(
                          "%I:%M%p").lstrip('0') + ' - ' +
                      self.event.l_end_date.strftime("%I:%M%p").lstrip('0') +
                      '</p>')
 
-        if self.event.location.count():
+        if self.event.location.count():  # where
             self.where = '<p><b>Where:</b> '
             for l in self.event.location.all():
                 self.where += l.name
@@ -49,31 +67,20 @@ class EventCalendar(LocaleHTMLCalendar):
         else:
             self.where = ''
 
+        # description
         self.desc = '<p><b>Description:</b> ' + self.event.description[:100]
         self.desc += ('...</p>' if len(self.event.description) > 100
                       else '</p>')
 
-        self.event_url = self.event.get_absolute_url()
+        self.event_url = self.event.get_absolute_url()  # url
         t = "%I:%M%p" if self.event.l_start_date.minute else "%I%p"
         self.title2 = (self.event.l_start_date.strftime(t).lstrip('0') +
                        ' ' + self.title)
 
     def formatday(self, day, weekday):
         """Return a day as a table cell."""
+        super(EventCalendar, self).formatday(day, weekday)
         self.day = day
-
-        wkday_not_today = ('<td class="%s"><div class="td-inner">'
-                           % self.cssclasses[weekday])
-
-        wkday_today = ('<td class="%s calendar-today"><div class="td-inner">'
-                       % self.cssclasses[weekday])
-
-        end = '</div></td>'
-
-        anch = '<a href="/%s/%d/%d/%d/">%d</a>' % (
-            URL, self.yr, self.mo, day, day
-        )
-
         out = ''
 
         if day == 0:
@@ -81,14 +88,14 @@ class EventCalendar(LocaleHTMLCalendar):
         elif now.month == self.mo and now.year == self.yr and day == now.day:
             if day in self.count:
                 # don't return just yet
-                out = wkday_today + anch
+                out = self.wkday_today + self.anch
             else:
-                return wkday_today + anch + end
+                return self.wkday_today + self.anch + self.end
         elif day in self.count:
             # don't return just yet
-            out = wkday_not_today + anch
+            out = self.wkday_not_today + self.anch
         else:
-            return wkday_not_today + anch + end
+            return self.wkday_not_today + self.anch + self.end
 
         detail = "%s%s%s<br><a href='%s'>View details</a>"
         extras = ('<div title="%s" data-content="%s" data-container="body"'
@@ -116,7 +123,7 @@ class EventCalendar(LocaleHTMLCalendar):
                     ) +
                     self.title2 + '</div></a>')
 
-        return out + end
+        return out + self.end
 
     def formatmonthname(self, theyear, themonth, withyear=True):
         """
@@ -132,28 +139,13 @@ class EventCalendar(LocaleHTMLCalendar):
                 'Today</button> %s</th></tr>' % s)
 
 
-class MiniEventCalendar(LocaleHTMLCalendar):
-    def __init__(self, year, month, count, events, *args):
+class MiniEventCalendar(GenericCalendar):
+    def __init__(self, *args):
         super(MiniEventCalendar, self).__init__(*args)
-        self.yr = year
-        self.mo = month
-        self.count = dict(count)
-        self.events = events
-
-    def add_occurrence(self):
-        try:
-            self.event.occurrence.append(self.day)
-        except AttributeError:
-            self.event.occurrence = []
-            self.event.occurrence.append(self.day)
-
-    def check_if_cancelled(self):
-        if self.event.cancellations.count():
-            cancellations = self.event.cancellations.all()
-            d = date(self.yr, self.mo, self.day)
-            for cancellation in cancellations:
-                if cancellation.date == d:
-                    self.title += " (CANCELLED)"
+        # Change count from a defaultdict to a regular dict, so that when we
+        # try and check if there are days in count, they won't be added if they
+        # aren't there.
+        self.count = dict(self.count)
 
     def popover_helper(self):
         num_events = len(self.count[self.day])
@@ -177,34 +169,22 @@ class MiniEventCalendar(LocaleHTMLCalendar):
 
     def formatday(self, day, weekday):
         """Return a day as a table cell."""
+        super(MiniEventCalendar, self).formatday(day, weekday)
         self.day = day
-        wkday_not_today = '<td class="%s"><div class="td-inner">' % (
-            self.cssclasses[weekday])
-
-        wkday_today = (
-            '<td class="%s calendar-today"><div class="td-inner">' % (
-                self.cssclasses[weekday])
-        )
-
-        anch = '<a href="/%s/%d/%d/%d">%d</a>' % (
-            URL, self.yr, self.mo, day, day
-        )
-
-        end = '</div></td>'
 
         if day == 0:
             return '<td class="noday">&nbsp;</td>'  # day outside month
         elif now.month == self.mo and now.year == self.yr and day == now.day:
             if day in self.count:
                 self.popover_helper()
-                return wkday_today + anch + self.cal_event + end
+                return self.wkday_today + self.anch + self.cal_event + self.end
             else:
-                return wkday_today + anch + end
+                return self.wkday_today + self.anch + self.end
         elif day in self.count:
             self.popover_helper()
-            return wkday_not_today + anch + self.cal_event + end
+            return self.wkday_not_today + self.anch + self.cal_event + self.end
         else:
-            return wkday_not_today + anch + end
+            return self.wkday_not_today + self.anch + self.end
 
     def formatmonthname(self, theyear, themonth, withyear=True):
         """
