2fe932f06326cf07d23f19fc6c2c4eb6dce69c05
Reuben Urbina <reubenurbina@gmail.com>
2014-06-16 13:07:27 -0700

    added tests to check for proper event ordering.

---
+++ tests/unit_tests/test_ordering.py
@@ -0,0 +1,72 @@
+from __future__ import unicode_literals
+
+import datetime
+
+from django.utils.timezone import make_aware, get_default_timezone
+from django.core.urlresolvers import reverse
+from django.utils.encoding import force_bytes
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ..integration_tests.event_factory import SetMeUp
+from happenings.models import Event
+
+
+class EventOrderingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'foo', 'bar@example.com', 'secret'
+        )
+        d1 = make_aware(
+            datetime.datetime(2014, 5, 20, 5), get_default_timezone()
+        )
+        d2 = make_aware(
+            datetime.datetime(2014, 5, 20, 6), get_default_timezone()
+        )
+        d3 = make_aware(
+            datetime.datetime(2014, 5, 20, 7), get_default_timezone()
+        )
+        self.event1 = Event.objects.create(
+            start_date=d1,
+            end_date=d2,
+            all_day=True,
+            created_by=self.user,
+            title="The big event",
+            description="Amazing event",
+            repeat="NEVER",
+        )
+        self.event2 = Event.objects.create(
+            start_date=d2,
+            end_date=d3,
+            all_day=True,
+            created_by=self.user,
+            title="The next big event",
+            description="Amazing next event",
+            repeat="NEVER",
+        )
+
+    def assertContentBefore(self, response, text1, text2, failing_msg=None):
+        """
+        Testing utility asserting that text1 appears before text2 in response
+        content. Modified from:
+        https://github.com/django/django/blob/master/tests/admin_views/tests.py
+        """
+        if failing_msg is None:
+            failing_msg="%s not found before %s" % (text1, text2)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            response.content.index(force_bytes(text1)) <
+            response.content.index(force_bytes(text2)),
+            failing_msg)
+
+    def test_ordering_month_view(self):
+        response = self.client.get(reverse(
+            'calendar:list', kwargs={'year': '2014', 'month': '5'}
+        ))
+        self.assertContentBefore(response, self.event1.title, self.event2.title)
+
+    def test_ordering_day_view(self):
+        response = self.client.get(reverse(
+            'calendar:day_list', kwargs={'year': '2014', 'month': '5', 'day': '20'}
+        ))
+        self.assertContentBefore(response, self.event1.title, self.event2.title)
